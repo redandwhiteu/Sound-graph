f42027dc8b00a4185d17d58ee9ee48897bf9b917
Redandwhiteu <redandwhiteu@gmail.com>
2025-06-14 15:34:57 +0500

    Функционал звукозаписи

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import sys
-import pyaudio
 
+import sounddevice as sd
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import *
+from src.recording import stop_recording, recording
+
 
 class Main(QMainWindow):
     MODELS_INTERFACE_BLOCK = ['model_type_box', 'rec_type_label']
-    PC_SOUNDS_INTERFACE_BLOCK = ['rec_type_box', 'rec_type_label', 'start_btn', 'stop_btn']
+    PC_SOUNDS_INTERFACE_BLOCK = ['rec_type_box', 'rec_type_label', 'start_btn', 'reg_seconds']
     FILES_INTERFACE_BLOCK = ['path_label', 'path_sound_label', 'path_btn']
     METHOD_PARAMETERS_BLOCK = ['reg_1', 'reg_label_1', 'reg_2', 'reg_label_2', 'reg_3', 'reg_label_3',
-                              'reg_4', 'reg_label_4', 'reg_5', 'reg_label_5']
+                               'reg_4', 'reg_label_4', 'reg_5', 'reg_label_5']
 
     def __init__(self):
         super(Main, self).__init__()
@@ -18,6 +20,7 @@ class Main(QMainWindow):
         self.path_btn.clicked.connect(self.getting_file_path)
         self.input_type_box.currentIndexChanged.connect(lambda: self.choise_event(0))
         self.rec_type_box.currentIndexChanged.connect(lambda: self.choise_event(1))
+        self.start_btn.clicked.connect(lambda: recording(self.get_index(), self.reg_seconds.text()))
 
     def getting_file_path(self):
         self.path_sound_label.setText(QtWidgets.QFileDialog.getOpenFileName()[0])
@@ -52,21 +55,29 @@ class Main(QMainWindow):
                         eval('self.' + item + '.hide()')
                 for item in self.PC_SOUNDS_INTERFACE_BLOCK:
                     eval('self.' + item + '.show()')
-                self.line_4.setGeometry(10, 195, 1, 140)
-                self.line_5.setGeometry(325, 195, 1, 140)
-                self.line_6.setGeometry(10, 335, 316, 1)
-                self.graphics_btn.setGeometry(10, 340, 316, 30)
+                self.line_4.setGeometry(10, 195, 1, 165)
+                self.line_5.setGeometry(325, 195, 1, 165)
+                self.line_6.setGeometry(10, 360, 316, 1)
+                self.graphics_btn.setGeometry(10, 365, 316, 30)
                 self.rec_type_label.setText('Выберите устройство записи:')
                 self.choise_event(1)
         elif box_id == 1:
-            pya = pyaudio.PyAudio()
-            for inpt in range(pya.get_device_count()):
-                self.rec_type_box.addItem((pya.get_device_info_by_index(inpt)['name']))
+            devices = sd.query_devices()
+            for dev in devices:
+                if dev['max_input_channels'] >= 1:
+                    self.rec_type_box.addItem(dev['name'])
+
         else:
             if self.model_type_box.currentText() == 'a(t) = Acos(ωt + φ)':
                 pass
             else:
                 pass
+
+    def get_index(self):
+        devices = sd.query_devices()
+        for dev in devices:
+            if dev['name'] == self.input_type_box.currentText():
+                return dev['name']
 
 
 def except_hook(cls, exception, traceback):
---
+++ src/recording.py
@@ -0,0 +1,35 @@
+import pyaudio
+import wave
+
+RECORDING = True
+
+def recording(index, seconds):
+    global RECORDING
+    RECORDING = True
+    p = pyaudio.PyAudio()
+    stream = p.open(format = pyaudio.paInt16,
+                    channels = 2,
+                    rate = 44100,
+                    frames_per_buffer = 1024,
+                    input_device_index = index,
+                    input = True)
+
+    frames = []
+    for i in range(0, int((44100 / 1024) * int(seconds))):
+        data = stream.read(1024)
+        frames.append(data)
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    wf = wave.open('tmp/sound.wav', 'wb')
+    wf.setnchannels(2)
+    wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
+    wf.setframerate(44100)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+def stop_recording():
+    global RECORDING
+    RECORDING = False

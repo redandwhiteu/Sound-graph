79d093e71ef15bc1947b2d4a197c507470f257b6
Redandwhiteu <redandwhiteu@gmail.com>
2025-06-19 14:05:15 +0500

    Сохранение и выведение графиков

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import shutil
 import sys
 import os
 
@@ -19,16 +20,24 @@ class Main(QMainWindow):
                                'reg_4', 'reg_label_4', 'reg_5', 'reg_label_5']
 
     def __init__(self):
-        super(Main, self).__init__()
-        uic.loadUi('styles/interface.ui', self)
-        os.mkdir('tmp')
-        self.choise_event(0)
-        self.path_btn.clicked.connect(self.getting_file_path)
-        self.input_type_box.currentIndexChanged.connect(lambda: self.choise_event(0))
-        self.rec_type_box.currentIndexChanged.connect(lambda: self.choise_event(1))
-        self.start_btn.clicked.connect(lambda: self.path_sound_label.setText(recording(self.get_index(),
+        self.path = None
+        try:
+            super(Main, self).__init__()
+            uic.loadUi('styles/interface.ui', self)
+            self.graph_window = None
+            try:
+                os.mkdir('tmp')
+            except:
+                pass
+            self.choise_event(0)
+            self.path_btn.clicked.connect(self.getting_file_path)
+            self.input_type_box.currentIndexChanged.connect(lambda: self.choise_event(0))
+            self.rec_type_box.currentIndexChanged.connect(lambda: self.choise_event(1))
+            self.start_btn.clicked.connect(lambda: self.path_sound_label.setText(recording(self.get_index(),
                                                                                        self.reg_seconds.text())))
-        self.graphics_btn.clicked.connect(lambda: self.drawing())
+            self.graphics_btn.clicked.connect(lambda: self.drawing())
+        except Exception as e:
+            print(f'Ошибка запуска основного окна: {e}')
 
     def getting_file_path(self):
         self.path_sound_label.setText(QtWidgets.QFileDialog.getOpenFileName()[0])
@@ -45,18 +54,32 @@ class Main(QMainWindow):
                 self.line_4.setGeometry(10, 195, 1, 195)
                 self.line_5.setGeometry(325, 195, 1, 195)
                 self.line_6.setGeometry(10, 390, 316, 1)
-                self.graphics_btn.setGeometry(10, 395, 316, 30)
+                self.graphics_btn.setGeometry(10, 510, 316, 30)
                 self.rec_type_label.setText('Выберите аудиомодель:')
+                self.line_7.setGeometry(10, 400, 316, 1)
+                self.line_8.setGeometry(10, 400, 1, 110)
+                self.line_9.setGeometry(325, 400, 1, 110)
+                self.line_10.setGeometry(10, 510, 316, 1)
+                self.sopt_label_3.setGeometry(2, 395, 330, 25)
+                self.output_type_label.setGeometry(2, 415, 330, 25)
+                self.output_type_box.setGeometry(15, 440, 305, 50)
             elif self.input_type_box.currentText() == 'Файл':
                 for box in self.PC_SOUNDS_INTERFACE_BLOCK, self.MODELS_INTERFACE_BLOCK, self.METHOD_PARAMETERS_BLOCK:
                     for item in box:
                         eval('self.' + item + '.hide()')
                 for item in self.FILES_INTERFACE_BLOCK:
                     eval('self.' + item + '.show()')
-                self.line_4.setGeometry(10, 195, 1, 110)
-                self.line_5.setGeometry(325, 195, 1, 110)
-                self.line_6.setGeometry(10, 305, 316, 1)
-                self.graphics_btn.setGeometry(10, 310, 316, 30)
+                self.line_4.setGeometry(10, 195, 1, 75)
+                self.line_5.setGeometry(325, 195, 1, 75)
+                self.line_6.setGeometry(10, 270, 316, 1)
+                self.graphics_btn.setGeometry(10, 415, 316, 30)
+                self.line_7.setGeometry(10, 305, 316, 1)
+                self.line_8.setGeometry(10, 305, 1, 105)
+                self.line_9.setGeometry(325, 305, 1, 105)
+                self.line_10.setGeometry(10, 410, 316, 1)
+                self.sopt_label_3.setGeometry(2, 295, 330, 25)
+                self.output_type_label.setGeometry(2, 315, 330, 25)
+                self.output_type_box.setGeometry(15, 340, 305, 50)
             else:
                 for box in self.MODELS_INTERFACE_BLOCK, self.FILES_INTERFACE_BLOCK, self.METHOD_PARAMETERS_BLOCK:
                     for item in box:
@@ -66,8 +89,15 @@ class Main(QMainWindow):
                 self.line_4.setGeometry(10, 195, 1, 165)
                 self.line_5.setGeometry(325, 195, 1, 165)
                 self.line_6.setGeometry(10, 360, 316, 1)
-                self.graphics_btn.setGeometry(10, 365, 316, 30)
+                self.graphics_btn.setGeometry(10, 485, 316, 30)
                 self.rec_type_label.setText('Выберите устройство записи:')
+                self.line_7.setGeometry(10, 375, 316, 1)
+                self.line_8.setGeometry(10, 375, 1, 105)
+                self.line_9.setGeometry(325, 375, 1, 105)
+                self.line_10.setGeometry(10, 480, 316, 1)
+                self.sopt_label_3.setGeometry(2, 365, 330, 25)
+                self.output_type_label.setGeometry(2, 385, 330, 25)
+                self.output_type_box.setGeometry(15, 410, 305, 50)
                 self.choise_event(1)
         elif box_id == 1:
             devices = sd.query_devices()
@@ -81,6 +111,7 @@ class Main(QMainWindow):
             else:
                 pass
 
+
     def get_index(self):
         devices = sd.query_devices()
         for dev in devices:
@@ -88,12 +119,41 @@ class Main(QMainWindow):
                 return dev['name']
 
     def drawing(self):
-        path_fq_sp = draw_frequency_spectrum(self.path_sound_label.text())
-        path_sp = draw_spectrogram(self.path_sound_label.text())
-        path_wave = draw_waveform(self.path_sound_label.text())
-        self.fq_sp.setPixmap(QPixmap(path_fq_sp))
-        self.sp.setPixmap(QPixmap(path_sp))
-        self.wf.setPixmap(QPixmap(path_wave))
+        if self.graph_window is None:
+            self.graph_window = GraphWindow(parent=self)
+        self.graph_window.show()
+        self.graph_window.raise_()
+        self.graph_window.activateWindow()
+        if self.output_type_box.currentText() == 'Амплитудный график':
+            self.path = draw_waveform(self.path_sound_label.text())
+        elif self.output_type_box.currentText() == 'Спектрограмма':
+            self.path = draw_spectrogram(self.path_sound_label.text())
+        else:
+            self.path = draw_frequency_spectrum(self.path_sound_label.text())
+        self.graph_window.graph.setPixmap(QPixmap(self.path))
+
+
+class GraphWindow(QWidget):
+    def __init__(self, parent=None):
+        self.parent_window = parent
+        super(GraphWindow, self).__init__()
+        uic.loadUi('styles/graph.ui', self)
+
+        self.save_btn.clicked.connect(lambda: self.save_graph())
+
+    def save_graph(self):
+        main = Main()
+        target_path, _ = QFileDialog.getSaveFileName(
+            self,
+            'Сохранить как...',
+            'Graph.png',
+            'PNG файлы (*.png);;Все файлы (*)'
+        )
+        if target_path:
+            try:
+                shutil.copy(self.parent_window.path, target_path)
+            except Exception as e:
+                print(f'Ошибка при копировании: {e}')
 
 
 def except_hook(cls, exception, traceback):
